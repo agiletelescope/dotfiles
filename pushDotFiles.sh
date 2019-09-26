af2b5ed776a38853557657e4c149479a8ab11025
agiletelescope <agiletelescope@gmial.com>
2019-09-26 16:40:03 +0530

    added all dot files

--- pushDotFiles.sh
+++ pushDotFiles.sh
@@ -1,15 +1,15 @@
 # Copy all the files
 # Tried to do this using links, it doesn't work
 echo "Copying dotfiles ..."
-cp -fr ~/.vimrc vimrc 2> /dev/null
-cp -fr ~/.i3/config i3config 2> /dev/null
-cp -fr ~/.config/i3status/config i3status 2> /dev/null
-cp -fr ~/.bashrc bashrc 2> /dev/null
-cp -fr ~/.bash_aliases bash_aliases 2> /dev/null
+cp -fr ~/.vimrc vimrc &> /dev/null
+cp -fr ~/.i3/config i3config &> /dev/null
+cp -fr ~/.config/i3status/config i3status &> /dev/null
+cp -fr ~/.bashrc bashrc &> /dev/null
+cp -fr ~/.bash_aliases bash_aliases &> /dev/null
 
 # Push it to the repo
-echo "Git, adding all the files ..." 2> /dev/null
+echo "Git, adding all the files ..." &> /dev/null
 git add .
 echo "Git, Commiting files ..."
-git commit -m "added all dot files" 2> /dev/null
+git commit -m "added all dot files"
 git push
