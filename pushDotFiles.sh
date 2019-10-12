029b16d2c8dfcaf14bd01c6c2b928ac8af02c543
agiletelescope <agiletelescope@gmial.com>
2019-10-13 00:13:08 +0530

    added all dot files

--- pushDotFiles.sh
+++ pushDotFiles.sh
@@ -6,7 +6,6 @@ cp -fr ~/.i3/config i3config &> /dev/null
 cp -fr ~/.config/i3status/config i3status &> /dev/null
 cp -fr ~/.bashrc bashrc &> /dev/null
 cp -fr ~/.bash_aliases bash_aliases &> /dev/null
-cp -fr ~/.config/i3blocks . &> /dev/null
 cp -fr ~/.config/i3blocks/config i3blocksConfig &> /dev/null
 cp -fr ~/.config/i3blocks/cj_fetch_data.py cj_api_i3blocks.py &> /dev/null
 
